d5d1033d0a4166e695813f17d3ad8c070689cad9
imrankambal <imran.kambal@stud.fh-campuswien.ac.at>
2021-06-01 23:08:38 +0200

    rewritten code

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -6,8 +6,46 @@ public class App {
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    private static final String VALID_PASSWORD_REGEX = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[()#$?!%/@]).{8,25}$";
+    public static boolean checkPassword(String password){
+        int increase_count = 0, consecutive_count = 0;
+        boolean consecutive = true, increasing = true;
+
+        if(!password.matches(VALID_PASSWORD_REGEX))
+            return false;
+        else {
+            for (int i = 0; i < password.length(); i++) {
+                if (Character.isDigit(password.charAt(i))) {
+                    for (int j = i + 1; j < password.length(); j++) {
+                        if (increase_count < 2) {
+                            if (Character.getNumericValue(password.charAt(i)) == Character.getNumericValue(password.charAt(j)) - 1) {
+                                increase_count++;
+                                break;
+                            }
+                        }
+                        else
+                            increasing = false;
+                    }
+                    if (Character.isDigit(password.charAt(i))){
+                        for (int j = i + 1; j < password.length(); j++) {
+                            if (consecutive_count < 3) {
+                                if (Character.getNumericValue(password.charAt(i)) == Character.getNumericValue(password.charAt(j))) {
+                                    consecutive_count++;
+                                    break;
+                                }
+                            }
+                            else
+                                consecutive = false;
+
+                        }
+                    }
+                }
+            }
+            return consecutive & increasing;
+        }
     }
 }
--- src/test/java/at/ac/fhcampuswien/AppTest.java
+++ src/test/java/at/ac/fhcampuswien/AppTest.java
@@ -1,95 +1,53 @@
 package at.ac.fhcampuswien;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest {
-    private static final String VALID_PASSWORD_REGEX_LENGTH =  "^.{8,25}$";
-    private static final String VALID_PASSWORD_REGEX_LETTERS =  "^(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]*$";
-    private static final String VALID_PASSWORD_REGEX_NUMBERS =  "^(?=.*[0-9])[a-zA-Z0-9]*$";
-    private static final String VALID_PASSWORD_REGEX_SYMBOLS =  ".*[()#$?!%/@].*";
-
-    App a1 = new App();
-
     @Test
-    @DisplayName("In this test case the password length is checked; max min 8 max 25 characters;")
-    public void checkPasswordLength(){
-        a1.setPassword("15678");
-        boolean actual = a1.password.matches(VALID_PASSWORD_REGEX_LENGTH);
+    public void checkPasswordLong(){
+        String password = "23111dHasasdasdasd32e123134124124124124124#";
 
-        assertTrue(actual);
+        assertEquals(true, App.checkPassword(password));
+    }
+    @Test
+    public void checkPasswordShort(){
+        String password = "231";
+
+        assertEquals(true, App.checkPassword(password));
+    }
+    @Test
+    public void checkConsecutiveLowerCase() {
+        String password = "AAAAAAAAA23114#";
+
+        assertEquals(true, App.checkPassword(password));
+    }
+    @Test
+    public void checkDigit() {
+        String password = "AAAAAAAAA#er#";
+
+        assertEquals(true, App.checkPassword(password));
+    }
+    @Test
+    public void checkConsecutiveUpperCase() {
+        String password = "aaaaaaa3114#";
+
+        assertEquals(true, App.checkPassword(password));
+    }
+    @Test
+    public void checkConsecutiveNumbers() {
+        String password = "2333abdseHas#";
+
+        assertEquals(true, App.checkPassword(password));
+    }
+    @Test
+    public void checkIncreasingNumbers() {
+        String password = "23abdseHas#";
+
+        assertEquals(true, App.checkPassword(password));
     }
 
-    @Test
-    @DisplayName("In this test case the password is checked for upper and lower case letters;")
-    public void checkPasswordLetters(){
-        a1.setPassword("Aasdd2324esadd3");
-        boolean actual = a1.password.matches(VALID_PASSWORD_REGEX_LETTERS);
-
-        assertTrue(actual);
-    }
-
-    @Test
-    @DisplayName("In this test case the password is checked for numbers;")
-    public void checkPasswordNumbers(){
-        a1.setPassword("nonum6beraddedtothis");
-        boolean actual = a1.password.matches(VALID_PASSWORD_REGEX_NUMBERS);
-
-        assertTrue(actual);
-    }
-
-    @Test
-    @DisplayName("In this test case the password is checked for specific symbols;")
-    public void checkPasswordSymbols(){
-        a1.setPassword("nonumbera!ddedtothis");
-        boolean actual = a1.password.matches(VALID_PASSWORD_REGEX_SYMBOLS);
-
-        assertTrue(actual);
-    }
-
-    @Test
-    @DisplayName("In this test case the password is checked for increasing numbers;")
-    public void checkIncreasingNumbers(){
-        a1.setPassword("nonumbera!ddedtothis");
-        boolean actual = true;
-        int count = 0;
-
-        for (int i = 0; i < a1.password.length(); i++) {
-            if (Character.isDigit(a1.password.charAt(i))){
-                for (int j = i + 1; j < a1.password.length(); j++) {
-                    if (count < 2) {
-                        if (Character.getNumericValue(a1.password.charAt(i)) == Character.getNumericValue(a1.password.charAt(j)) - 1) {
-                            count++;
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-        assertTrue(count < 2);
-    }
-
-    @Test
-    @DisplayName("In this test case the password is checked for consecutive numbers;")
-    public void checkConsecutiveNumbers(){
-        a1.setPassword("nonu12456777mbera!ddedtothis");
-        boolean actual = true;
-        int count = 0;
-
-        for (int i = 0; i < a1.password.length(); i++) {
-            if (Character.isDigit(a1.password.charAt(i))){
-                for (int j = i + 1; j < a1.password.length(); j++) {
-                    if (count < 3) {
-                        if (Character.getNumericValue(a1.password.charAt(i)) == Character.getNumericValue(a1.password.charAt(j))) {
-                            count++;
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-        assertTrue(count < 3);
-    }
 }
